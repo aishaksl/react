6739038a96ec9a33d716202c3690bf9ac6bc4d74
aishaksl <aisha.koseoglu@gmail.com>
2025-08-17 16:01:26 +0700

    feat: add remove functionality to Todo slice

--- todoapp-typescript/src/components/ToDo.tsx
+++ todoapp-typescript/src/components/ToDo.tsx
@@ -3,6 +3,9 @@ import { CiCircleRemove } from "react-icons/ci";
 import { CiCircleCheck } from "react-icons/ci";
 import { FaRegEdit } from "react-icons/fa";
 import type { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodo } from '../redux/todoSlice';
+
 
 interface TodoPropsType {
     todoProps: TodoType
@@ -10,6 +13,7 @@ interface TodoPropsType {
 
 function ToDo({ todoProps }: TodoPropsType) {
 
+    const dispatch = useDispatch()
     const { id, content } = todoProps
 
     return (
@@ -17,7 +21,7 @@ function ToDo({ todoProps }: TodoPropsType) {
         <div className='flex flex-row justify-between w-full border text-sm p-2 rounded-md'>
             <div className='text-sm w-full' > {content}</div>
             <div className='flex flex-row items-center space-x-3'>
-                <CiCircleRemove className="text-xl cursor-pointer" />
+                <CiCircleRemove onClick={() => dispatch(removeTodo(id))} className="text-xl cursor-pointer" />
                 <CiCircleCheck className="text-xl cursor-pointer" />
                 <FaRegEdit className='text-lg cursor-pointer' />
             </div>
