0e66cd85ff71620a4d8dcc87b328921a215e1a16
aishaksl <aisha.koseoglu@gmail.com>
2025-08-18 17:12:31 +0700

    feat: add useToggle custom hook and integrate hide/show functionality in App

--- 16.hooks/src/App.jsx
+++ 16.hooks/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 import useCounter from "./hooks/useCounter";
+import useToggle from "./hooks/useToggle";
 
 function App() {
   const { count, increase, decrease } = useCounter();
 
+  const { open, change } = useToggle();
+
   return (
-    <div>
-      {count}
-      <div>
-        <button onClick={increase}>Increase</button>
-        <button onClick={decrease}>Decrease</button>
+    <div className="space-y-8">
+      <div className="space-y-3">
+        {count}
+        <div>
+          <button onClick={increase}>Increase</button>
+          <button onClick={decrease}>Decrease</button>
+        </div>
+      </div>
+
+      <div className="space-y-5">
+        {open && (
+          <div className="w-md h-2xl border rounded-md bg-white text-black ">
+            box
+          </div>
+        )}
+        <button onClick={change}>{open ? " Hide" : "Show"}</button>
       </div>
     </div>
   );
--- 16.hooks/src/hooks/useCounter.jsx
+++ 16.hooks/src/hooks/useCounter.jsx
@@ -19,3 +19,12 @@ function useCounter() {
 }
 
 export default useCounter;
+
+// HOOKS
+/*
+	1.	Use state in functional components (useState)
+	2.	Handle side effects like API calls or timers (useEffect)
+	3.	Share logic easily across components via custom hooks
+	4.	Access context (useContext) for global data
+	5.	Work with mutable references (useRef) without re-rendering
+*/
